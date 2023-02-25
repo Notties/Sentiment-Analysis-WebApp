b43782fc32b80679a91532fbee04efe2494cb0bd
Knot <akthakorn@gmail.com>
2023-02-26 02:50:20 +0700

    update nav

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -1,6 +1,11 @@
 import useComponentVisible from "@/src/hooks/useComponentVisible";
-import { GoogleOutlined } from "@ant-design/icons";
-import { Button, Row } from "antd";
+import {
+  DownOutlined,
+  GoogleOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Button, Divider, Dropdown, MenuProps, Row, Space } from "antd";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -14,13 +19,31 @@ const Nav = () => {
     setIsComponentVisible(!isComponentVisible);
   };
 
+  const { data: session } = useSession();
+
+  const [loadings, setLoadings] = useState<boolean>(false);
+
+  const items: MenuProps["items"] = [
+    {
+      label: (
+        <div onClick={() => signOut()}>
+          <Row align={"middle"} gutter={3}>
+            <p className="mr-2">Sign out</p>
+            <LogoutOutlined />
+          </Row>
+        </div>
+      ),
+      key: "1",
+    },
+  ];
+
   return (
     <>
       <nav
-        className="bg-blue-0  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 border-gray-100 px-2 sm:px-4 py-2.5"
+        className="bg-blue-0  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 px-2 sm:px-4 py-2.5 border-b-2 border-gray-100"
         ref={ref}
       >
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <div className="container flex flex-wrap items-center justify-between mx-auto ">
           <Link href="/" className="flex items-center">
             <img
               src="logo.png"
@@ -33,92 +56,88 @@ const Nav = () => {
           </Link>
 
           <div className="flex md:order-2">
-            <Button className="mt-1 md:mt-0">
-              <Row align={"middle"}>
-                <img
-                  src="https://img.icons8.com/fluency/256/google-logo.png"
-                  alt="google"
-                  className="w-4 h-4 mr-2 ml-0 mt-0"
-                />
-                Sign In
-              </Row>
-            </Button>
+            <>
+              <Space>
+                {session && session.user ? (
+                  <>
+                    <Dropdown.Button
+                      icon={
+                        <ion-icon
+                          name="chevron-down-outline"
+                          class="mt-1"
+                        ></ion-icon>
+                      }
+                      loading={loadings}
+                      menu={{ items }}
+                    >
+                      <Row align={"middle"}>
+                        <img
+                          src={`${session.user.image}`}
+                          alt=""
+                          className="w-5 h-5 mr-2 ml-0 mt-0 max-[450px]:mr-0 rounded-full"
+                        />
+                        <div className="max-[450px]:hidden">
+                          {session.user.name}
+                        </div>
+                      </Row>
+                    </Dropdown.Button>
+                  </>
+                ) : (
+                  <Button onClick={() => signIn("google")}>
+                    <Row align={"middle"}>
+                      <img
+                        src="https://img.icons8.com/fluency/256/google-logo.png"
+                        alt="google"
+                        className="w-4 h-4 mr-2 ml-0 mt-0"
+                      />
+                      Sign in
+                    </Row>
+                  </Button>
+                )}
 
-            <button
-              type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
+                {/* Mobile burger */}
+                <button
+                  type="button"
+                  className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden 
+                hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200"
+                  onClick={toggleMenu}
+                >
+                  <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              </Space>
+            </>
           </div>
 
-          <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 ">
-            <ul className="hidden md:flex md:p-4 md:flex-row md:space-x-8 md:mt-0">
-              <li>
-                <Link
-                  href="/"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                    md:hover:bg-transparent 
-                    md:hover:text-blue-700 
-                    md:p-0 
-                    md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white 
-                    md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/apidocs"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                    md:hover:bg-transparent 
-                    md:hover:text-blue-700 
-                    md:p-0 
-                    md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white 
-                    md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
-          {/* Mobile View */}
           {(isMenuOpen && isComponentVisible) ||
           (!isMenuOpen && isComponentVisible) ? (
+            /* Mobile View */
             <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 ">
               <ul
-                className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 
+                className="flex flex-col p-4 mt-4 border border-gray-200 rounded-lg bg-gray-50 
               md:flex-row 
               md:space-x-8 
               md:mt-0 
               md:text-sm 
               md:font-medium 
               md:border-0 
-              md:bg-white dark:bg-gray-800 
-              md:dark:bg-gray-900 dark:border-gray-700"
+              md:bg-white"
               >
                 <li>
                   <Link
                     href="/"
-                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 
-                    md:hover:bg-transparent 
-                    md:hover:text-blue-700 
-                    md:p-0 
-                    md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white 
-                    md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 hover:text-gray-500"
                   >
                     Home
                   </Link>
@@ -126,14 +145,42 @@ const Nav = () => {
                 <li>
                   <Link
                     href="/apidocs"
-                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 hover:text-gray-500"
                   >
-                    About
+                    API Docs
                   </Link>
                 </li>
               </ul>
             </div>
-          ) : null}
+          ) : (
+            /* Desktop View */
+            <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 ">
+              <ul className="hidden md:flex md:p-4 md:flex-row md:space-x-8 md:mt-0">
+                <li>
+                  <Link
+                    href="/"
+                    className="block py-2 pl-3 pr-4 text-gray-500 rounded duration-700
+                  md:hover:bg-transparent 
+                md:hover:text-blue-500 
+                  md:p-0 "
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/apidocs"
+                    className="block py-2 pl-3 pr-4 text-gray-500 rounded duration-700
+                  md:hover:bg-transparent 
+                md:hover:text-blue-500 
+                  md:p-0 "
+                  >
+                    API Docs
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </>
