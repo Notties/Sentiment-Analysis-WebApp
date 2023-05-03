aa622540d53b69afd305d64d62aa1f1d44b87bc8
Akthakorn Traimitr <akthakorn@gmail.com>
2023-05-04 01:53:57 +0700

    update api getuserId

--- src/pages/api/userId.ts
+++ src/pages/api/userId.ts
@@ -1,17 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../api/auth/[...nextauth]";
 const getUserIdByEmail = require("@/lib/prisma/users").getUserIdByEmail;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getServerSession(req, res, authOptions);
+  if (req.method == "POST") {
+    const userId = await getUserIdByEmail(req.body.email.toString());
 
-  console.log("userData", session?.user?.email);
-  console.log("getUserIdByEmail", getUserIdByEmail(session?.user?.email));
-
-  return res.status(200).json({ userId: session?.user?.email });
+    return res.status(200).json({ userId});
+  }
 }
